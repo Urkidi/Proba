8c86f458d33d1869772d57d84f4359f4e8eb141b
Unai Urkidi <uurkidi@gmail.com>
2013-09-18 11:22:53 +0200

    Hello world egina

---
+++ EGit proba/src/probapakete/Egunon.java
@@ -0,0 +1,17 @@
+package probapakete;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Egunon extends JFrame{
+	public Egunon() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello world!");
+
+	}
+
+}
